948aabd77067d735219347f9fd6767347d1bc926
SaugatBhattaraii <bhattaraisaugat39@gmail.com>
2024-05-23 11:39:48 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountName = "Saugat"
+var accountEmail = "saugat@gmail.com"
+accountCity = "Biratnagar"
+let accountState
+// accountId= 21
+/*
+Prefer not to use var because it doesnt define scope
+*/
+accountEmail= "ok@gmail.com"
+accountName="rony"
+accountCity="Itahari"
+console.log(accountId);
+console.table([accountEmail, accountName, accountCity, accountState])
