35a6458739a67b033b7a736215c205b822542a63
SaugatBhattaraii <bhattaraisaugat39@gmail.com>
2024-05-25 08:05:05 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high and this is not a good practice
+
+console.log("Saugat")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false //for example while logging in website whether the user is logged on or not
+// null => standalone value
+// undefined => 
+// symbol => unique //this will use a lot
+
+
+// object
+
+console.log(typeof undefined); // this type is undefined
+console.log(typeof null); // null type is object(Interview question)
